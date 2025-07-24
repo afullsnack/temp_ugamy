323866869818206b491cac4bcc6d78db1e824ae7
Miracle Friday <miraclef60@gmail.com>
2025-07-24 08:54:36 +0100

    refactor: Removed bootstrapped auth routes
    - Install dependencies

--- packages/api/src/app.ts
+++ packages/api/src/app.ts
@@ -1,6 +1,8 @@
+import { BetterAuthError } from "better-auth";
+
+import { auth } from "@/lib/auth";
 import configureOpenAPI from "@/lib/configure-open-api";
 import createApp from "@/lib/create-app";
-import authRoute from "@/routes/auth/auth.index";
 import index from "@/routes/index.route";
 
 const app = createApp();
@@ -10,11 +12,26 @@ configureOpenAPI(app);
 const routes = [
   index,
   // tasks,
-  authRoute,
 ] as const;
 
 routes.forEach((route) => {
   app.route("/", route);
+});
+app.on(["POST", "GET", "DELETE", "PUT"], "/api/auth/*", async (c) => {
+  try {
+    return await auth.handler(c.req.raw).catch((error) => {
+      if (error instanceof BetterAuthError) {
+        console.log("BetterAuth error", error);
+      }
+      console.log("Hono error", error);
+    });
+  }
+  catch (error: any) {
+    if (error instanceof BetterAuthError) {
+      console.error("BetterAuth error", error);
+    }
+    console.error("Hono error", error);
+  }
 });
 
 export type AppType = typeof routes[number];
