147ffbabdea961ebf8c7492945801e1e7a9e3747
Miracle Friday <miraclef60@gmail.com>
2025-07-24 08:53:34 +0100

    refactor: Fix auth handler and plugins

--- packages/api/src/lib/auth.ts
+++ packages/api/src/lib/auth.ts
@@ -1,14 +1,46 @@
 import { betterAuth } from "better-auth";
 import { drizzleAdapter } from "better-auth/adapters/drizzle";
+import { phoneNumber, username } from "better-auth/plugins";
 
 import db from "@/db";
+import * as authSchema from "@/db/schema/auth-schema";
 
 export const auth = betterAuth({
   basePath: "/api/auth",
   database: drizzleAdapter(db, {
     provider: "sqlite",
+    schema: { ...authSchema },
   }),
   emailAndPassword: {
     enabled: true,
+    minPasswordLength: 5,
+    requireEmailVerification: false,
+    revokeSessionsOnPasswordReset: false,
+    sendResetPassword: async (ctx, request) => { },
   },
+  emailVerification: {
+    sendVerificationEmail: async (ctx, request) => {
+
+    },
+    onEmailVerification: async (user, request) => {
+      console.log(`${user.name} with email ${user.email} has been verified`);
+    },
+  },
+  // hooks: {
+  //   before: async (context) => {
+  //   }
+  // },
+  plugins: [
+    username({
+      usernameValidator: (username) => {
+        console.log('Username', username);
+        if (username === "admin") {
+          return false;
+        }
+        return true;
+      },
+    }),
+    phoneNumber(),
+  ],
+  trustedOrigins: ["*", "http://localhost:3000"],
 });
--- packages/api/src/lib/create-app.ts
+++ packages/api/src/lib/create-app.ts
@@ -1,6 +1,7 @@
 import type { Schema } from "hono";
 
 import { OpenAPIHono } from "@hono/zod-openapi";
+import { cors } from "hono/cors";
 import { requestId } from "hono/request-id";
 import { notFound, onError, serveEmojiFavicon } from "stoker/middlewares";
 import { defaultHook } from "stoker/openapi";
@@ -20,10 +21,20 @@ export default function createApp() {
   const app = createRouter();
   app.use(requestId())
     .use(serveEmojiFavicon("📝"))
-    .use(pinoLogger());
+    .use(pinoLogger())
+    .use(cors({
+      origin: ["*", "http://localhost:3000"],
+      credentials: true,
+    }));
 
   app.notFound(notFound);
   app.onError(onError);
+  app.use(async (c, next) => {
+    await next();
+    if(c.res.status >= 400) {
+      console.log("Catch all 4xx error", JSON.stringify(c.res, null, 4));
+    }
+  })
   return app;
 }
 
