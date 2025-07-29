a63041a118903a0c31a9ca600118f926d12b6cf1
Miracle Friday <miraclef60@gmail.com>
2025-07-29 16:04:42 +0100

    refactor: Update dialect in drizzle.config.ts

--- packages/api/drizzle.config.ts
+++ packages/api/drizzle.config.ts
@@ -5,8 +5,7 @@ import env from "@/env";
 export default defineConfig({
   schema: env.NODE_ENV === "development"? "./src/db/schema" : "./dist/src/db/schema",
   out: env.NODE_ENV === "development"? "./src/db/migrations" : "./dist/src/db/migrations",
-  dialect: "sqlite",
-  driver: "turso",
+  dialect: env.NODE_ENV === "development"? "sqlite" : "turso",
   dbCredentials: {
     url: env.DATABASE_URL,
     authToken: env.DATABASE_AUTH_TOKEN,
