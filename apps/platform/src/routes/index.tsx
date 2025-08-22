fc3e3f70c3ca70662e657a6bc13b7ceaa49f6850
code-monarch <adiejoel14+monarch@gmail.com>
2025-08-22 11:35:54 +0100

    chore: update env

--- apps/platform/src/routes/index.tsx
+++ apps/platform/src/routes/index.tsx
@@ -1,18 +1,17 @@
 "use client"
 
-import { createFileRoute } from '@tanstack/react-router'
-import DashboardTemp from '@/components/dashboard/dashboard-temp'
-import Topbar from '@/components/common/topbar'
+import { createFileRoute, redirect } from '@tanstack/react-router'
 
 export const Route = createFileRoute('/')({
+  beforeLoad: () => {
+    throw redirect({ to: '/signin' })
+  },
   component: App,
 })
 
 function App() {
   return (
     <div className='relative bg-gray-100 w-full min-h-screen h-fit'>
-      <Topbar />
-      <DashboardTemp />
     </div>
   )
 }
