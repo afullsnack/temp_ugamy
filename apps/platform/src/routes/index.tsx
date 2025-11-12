92e7f802c6260487519c033983d774ba723dfcb7
code-monarch <adiejoel14+monarch@gmail.com>
2025-11-12 15:12:49 +0100

    change redirect

--- apps/platform/src/routes/index.tsx
+++ apps/platform/src/routes/index.tsx
@@ -4,7 +4,7 @@ import { createFileRoute, redirect } from '@tanstack/react-router'
 
 export const Route = createFileRoute('/')({
   beforeLoad: () => {
-    throw redirect({ to: '/signin' })
+    throw redirect({ to: '/dashboard' })
   },
   component: App,
 })
