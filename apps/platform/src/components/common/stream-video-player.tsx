3a137c26ba9d96e12b41eced768ef917147e5eb4
jesudara jeminusi <jesudara.j@gmail.com>
2026-01-22 21:37:29 +0000

    fix: isolated the solution

--- apps/platform/src/components/common/stream-video-player.tsx
+++ apps/platform/src/components/common/stream-video-player.tsx
@@ -125,7 +125,7 @@ export const StreamVideoPlayer = ({ videoId, userId, playlist = [] }: VideoPlaye
     const playbackSpeeds = [0.25, 0.5, 0.75, 1, 1.25, 1.5, 1.75, 2]
 
     const queryClient = useQueryClient()
-    // const isiOS = /iPad|iPhone|iPod/.test(navigator.userAgent)
+    const isiOS = /iPad|iPhone|iPod/.test(navigator.userAgent)
 
 
     // Get Video by Id
@@ -768,6 +768,7 @@ export const StreamVideoPlayer = ({ videoId, userId, playlist = [] }: VideoPlaye
                     onProgress={handleProgress}
                     onContextMenu={(e) => e.preventDefault()}
                     onDragStart={(e) => e.preventDefault()}
+                    controls={isiOS}
                 >
                     <source src={`${apiUrl}/videos/stream/${video.key.split("/").pop()}`} type="video/mp4" />
                     Your browser does not support the video tag.
