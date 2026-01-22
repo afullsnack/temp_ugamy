3f622d200f7a7c0ac8a1f039ee9b48bd992500f0
jesudara jeminusi <jesudara.j@gmail.com>
2026-01-22 21:29:40 +0000

    refactor: test to see what made video play on iphone

--- apps/platform/src/components/common/stream-video-player.tsx
+++ apps/platform/src/components/common/stream-video-player.tsx
@@ -125,7 +125,7 @@ export const StreamVideoPlayer = ({ videoId, userId, playlist = [] }: VideoPlaye
     const playbackSpeeds = [0.25, 0.5, 0.75, 1, 1.25, 1.5, 1.75, 2]
 
     const queryClient = useQueryClient()
-    const isiOS = /iPad|iPhone|iPod/.test(navigator.userAgent)
+    // const isiOS = /iPad|iPhone|iPod/.test(navigator.userAgent)
 
 
     // Get Video by Id
@@ -686,10 +686,10 @@ export const StreamVideoPlayer = ({ videoId, userId, playlist = [] }: VideoPlaye
         }
 
         document.addEventListener("visibilitychange", handleVisibilityChange)
-        if (!isiOS) {
-            window.addEventListener("blur", handleBlur)
-          }        
-          document.addEventListener("contextmenu", handleContextMenu)
+        // if (!isiOS) {
+        //     window.addEventListener("blur", handleBlur)
+        //   }        
+        document.addEventListener("contextmenu", handleContextMenu)
 
         return () => {
             document.removeEventListener("visibilitychange", handleVisibilityChange)
@@ -768,7 +768,6 @@ export const StreamVideoPlayer = ({ videoId, userId, playlist = [] }: VideoPlaye
                     onProgress={handleProgress}
                     onContextMenu={(e) => e.preventDefault()}
                     onDragStart={(e) => e.preventDefault()}
-                    controls={isiOS}
                 >
                     <source src={`${apiUrl}/videos/stream/${video.key.split("/").pop()}`} type="video/mp4" />
                     Your browser does not support the video tag.
